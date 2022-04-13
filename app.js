b25a1242153aa7b7cb9109e7be37912b5973f58a
Rishu Singh <79107856+Rishusing@users.noreply.github.com>
2022-04-14 01:50:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routers/route')
-const cors = require("cors");
+// const cors = require("cors");
 
 
 const port = process.env.PORT || 5000
@@ -10,7 +10,7 @@ const app = express()
 dotenv.config({path: './config.env'})
 require('./db/conn')
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
 app.use("/api", authRoutes);
 
